058543be8667ae718eaabb702f4f3a469fe19592
frankieolguin <141462017+frankieolguin@users.noreply.github.com>
2025-02-18 03:17:22 +0000

    attempting to submit!

---
+++ Assignments/IntroToProgramming/fahrenheit_converter/target/debug/deps/fahrenheit_converter-75d8af4162518f6d.d
@@ -0,0 +1,5 @@
+/workspaces/rustSPRING2025/Assignments/IntroToProgramming/fahrenheit_converter/target/debug/deps/fahrenheit_converter-75d8af4162518f6d: src/main.rs
+
+/workspaces/rustSPRING2025/Assignments/IntroToProgramming/fahrenheit_converter/target/debug/deps/fahrenheit_converter-75d8af4162518f6d.d: src/main.rs
+
+src/main.rs:
---
+++ Assignments/IntroToProgramming/odd_even/target/debug/deps/odd_even-eb13c0de29a15b95.d
@@ -0,0 +1,5 @@
+/workspaces/rustSPRING2025/Assignments/IntroToProgramming/odd_even/target/debug/deps/odd_even-eb13c0de29a15b95: src/main.rs
+
+/workspaces/rustSPRING2025/Assignments/IntroToProgramming/odd_even/target/debug/deps/odd_even-eb13c0de29a15b95.d: src/main.rs
+
+src/main.rs:
